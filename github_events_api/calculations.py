74cf54501727d04ad5ae0122669f25852cfb35cc
ivana.marosova <ivana.marosova@workday.com>
2024-08-31 15:05:28 +0200

    Move name strings to constants

--- github_events_api/calculations.py
+++ github_events_api/calculations.py
@@ -1,13 +1,20 @@
 import numpy as np
 import pandas as pd
 
+from github_events_api.constants import (
+    EVENT_AVG_TIME_DIFF,
+    EVENT_CREATED_AT,
+    EVENT_REPO_ID,
+    EVENT_TIME_DIFF,
+    EVENT_TYPE,
+)
 from github_events_api.data_storage import Event
 
 
 def load_events_data_into_df(events: list[Event]) -> pd.DataFrame:
     """Transform list of Events into dataframe for further analysis."""
     events_df = pd.DataFrame.from_records([e.model_dump() for e in events])
-    events_df["created_at"] = pd.to_datetime(events_df["created_at"])
+    events_df[EVENT_CREATED_AT] = pd.to_datetime(events_df[EVENT_CREATED_AT])
 
     return events_df
 
@@ -21,15 +28,15 @@ def _calculate_rolling_average_time_diff(events_group: pd.DataFrame) -> float:
     """
     # select relevant events
     # either last 7 days or 500 events, which one is sooner
-    last_date = events_group["created_at"].max()
+    last_date = events_group[EVENT_CREATED_AT].max()
     start_date = last_date - pd.Timedelta(days=7)
-    date_mask = events_group["created_at"] >= start_date
+    date_mask = events_group[EVENT_CREATED_AT] >= start_date
 
     eligable_events = events_group.loc[date_mask].head(500)
     # get time difference between events
-    eligable_events["time_diff"] = eligable_events["created_at"].diff()
+    eligable_events[EVENT_TIME_DIFF] = eligable_events[EVENT_CREATED_AT].diff()
 
-    return eligable_events["time_diff"].mean().total_seconds()
+    return eligable_events[EVENT_TIME_DIFF].mean().total_seconds()
 
 
 def calculate_rolling_avg_time_diff_per_event_type(data: pd.DataFrame) -> pd.DataFrame:
@@ -41,17 +48,17 @@ def calculate_rolling_avg_time_diff_per_event_type(data: pd.DataFrame) -> pd.Dat
     first.
     """
     # sort events
-    data = data.sort_values(by=["repo_id", "type", "created_at"])
+    data = data.sort_values(by=[EVENT_REPO_ID, EVENT_TYPE, EVENT_CREATED_AT])
 
     # get time diff between consecutive events
     avg_time_diff_secs = (
-        data.groupby(["repo_id", "type"])
-        .apply(lambda x: pd.Series({"avg_time_diff_secs": _calculate_rolling_average_time_diff(x)}))
+        data.groupby([EVENT_REPO_ID, EVENT_TYPE])
+        .apply(lambda x: pd.Series({EVENT_AVG_TIME_DIFF: _calculate_rolling_average_time_diff(x)}))
         .reset_index()
     )
 
     # transform np.nan values in averages to None for later storage in db
-    avg_time_diff_secs["avg_time_diff_secs"] = avg_time_diff_secs["avg_time_diff_secs"].replace(
+    avg_time_diff_secs[EVENT_AVG_TIME_DIFF] = avg_time_diff_secs[EVENT_AVG_TIME_DIFF].replace(
         {np.nan: None}
     )
 
